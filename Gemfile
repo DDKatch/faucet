4e4ec91a0e3edd73c2c94f7938b77628a9039cfc
DDKatch <daniil.field@gmail.com>
2017-04-20 11:27:22 +0300

    Cap init

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+  gem 'capistrano', '~> 3.6'
+  gem 'capistrano-rails', '~> 1.2'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,66 +1,33 @@
-require 'pry'
-require 'mina/git'
-require 'mina/bundler'
-require 'mina/rails'
-require 'mina/rvm'
-require 'mina/rsync'
-require 'yaml'
+# config valid only for current version of Capistrano
+lock "3.8.0"
 
-DEPLOY_CONF = YAML.load(File.read('config/deploy.yml'))
+set :application, "my_app_name"
+set :repo_url, "git@example.com:me/my_repo.git"
 
-set :term_mode, nil
-set :domain, DEPLOY_CONF['host']
-set :user, DEPLOY_CONF['user']
-set :deploy_to, '/var/faucet'
-set :repository, DEPLOY_CONF['repository']
-set :branch, DEPLOY_CONF['branch']
-set :shared_paths, ['log', 'config/faucet.yml', 'config/secrets.yml', 'config/database.yml' ,'public/wallet', 'db/bitshares_faucet.sqlite3']
-set :rsync_options, %w[-az --force --recursive --delete --delete-excluded --progress --exclude-from=.gitignore --exclude 'public/*']
-set :wallet_params, DEPLOY_CONF['wallet_params']
-set :wallet_import, DEPLOY_CONF['wallet_import']
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-task :environment do
-  set :rvm_ruby_version, '2.2.3'
-end
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
 
-task :run_cli_wallet => :environment do
-  queue! %[echo "#{wallet_import}" | /usr/local/bin/cli_wallet #{wallet_params}]
-end
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-task :setup => :environment do
-    queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
-    queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-    queue! %[mkdir -p "#{deploy_to}/#{shared_path}/public/wallet"]
-    queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
-    queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
-end
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
-task :deploy => :environment do
-    deploy do
-        invoke :'rsync:deploy'
-        invoke :'deploy:link_shared_paths'
-        invoke :'bundle:install'
-        invole :'mina run_cli_wallet -d'
-        invoke :'rails:db_migrate'
-        invoke :'rails:assets_precompile'
-        to :launch do
-            queue "touch #{deploy_to}/tmp/restart.txt"
-        end
-    end
-end
+# Default value for :pty is false
+# set :pty, true
 
-task :restart do
-    queue 'sudo service nginx restart'
-end
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
-task :wallet do
-    $script =
-<<SCRIPT
-      echo 'deploying wallet';
-      rsync -az --force --delete --progress public/wallet/ #{domain}:#{deploy_to}/#{shared_path}/public/wallet;
-SCRIPT
-    exec $script
-    to :launch do
-        queue "touch #{deploy_to}/tmp/restart.txt"
-    end
-end
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
