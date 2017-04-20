2433339863a8d7402dfcc96e77ca12fe8f1e26d0
DDKatch <daniil.field@gmail.com>
2017-04-20 11:22:41 +0300

    Add cli task

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'pry'
 gem 'kaminari'
+gem 'puma'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,15 @@ set :repository, DEPLOY_CONF['repository']
 set :branch, DEPLOY_CONF['branch']
 set :shared_paths, ['log', 'config/faucet.yml', 'config/secrets.yml', 'config/database.yml' ,'public/wallet', 'db/bitshares_faucet.sqlite3']
 set :rsync_options, %w[-az --force --recursive --delete --delete-excluded --progress --exclude-from=.gitignore --exclude 'public/*']
+set :wallet_params, DEPLOY_CONF['wallet_params']
+set :wallet_import, DEPLOY_CONF['wallet_import']
 
 task :environment do
   set :rvm_ruby_version, '2.2.3'
+end
+
+task :run_cli_wallet => :environment do
+  queue! %[echo "#{wallet_import}" | /usr/local/bin/cli_wallet #{wallet_params}]
 end
 
 task :setup => :environment do
@@ -34,6 +40,7 @@ task :deploy => :environment do
         invoke :'rsync:deploy'
         invoke :'deploy:link_shared_paths'
         invoke :'bundle:install'
+        invole :'mina run_cli_wallet -d'
         invoke :'rails:db_migrate'
         invoke :'rails:assets_precompile'
         to :launch do
